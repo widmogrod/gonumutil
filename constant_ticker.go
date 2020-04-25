fbb944a209d24b60ce0c3c9587a801120fe95c3e
widmogrod <widmogrod@gmail.com>
2020-04-25 19:52:57 +0200

    Godoc

--- constant_ticker.go
+++ constant_ticker.go
@@ -1,3 +1,4 @@
+// Package provides utility functions for [GoNum](https://github.com/gonum)
 package gonumutil
 
 import (
@@ -5,10 +6,15 @@ import (
 	"gonum.org/v1/plot"
 )
 
+// NewConstantNumTicker simplify change of scale of the X and Y axis to have constant distance between markers.
+// step represents what distance between marks on a axis you want to preserve.
 func NewConstantNumTicker(step uint) plot.TickerFunc {
 	return NewConstantTicker("%1.0f", float64(step))
 }
 
+// NewConstantNumTicker simplify change of scale of the X and Y axis to have constant distance between markers.
+// label give you possibility to customize label. Accepts `Sprintf` formatting specification.
+// step represents what distance between marks on a axis you want to preserve.
 func NewConstantTicker(label string, step float64) plot.TickerFunc {
 	return plot.TickerFunc(func(min, max float64) []plot.Tick {
 		var res = make([]plot.Tick, 0)
---
+++ constant_ticker_example_test.go
@@ -0,0 +1,12 @@
+package gonumutil
+
+import "gonum.org/v1/plot"
+
+func ExampleConstantNumTicker() {
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+	p.X.Tick.Marker = NewConstantNumTicker(1)
+	p.Y.Tick.Marker = NewConstantNumTicker(5)
+}
